7afd20183c94a6b3bc6ba74f604c459a273f5a80
jp54438 <juha.pekka.p@gmail.com>
2017-10-03 13:57:25 +0300

    adding more user settings and print gameboard

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ var rows = 3
 var cols = 3
 var lineLength = 3
 var gameBoard = []
+var player1 = ''
+var player2 = ''
+var nbrPlayers = 1
 
 function main () {
   setupGame()
 }
 
 function setupGame () {
-  rows = ask('row', 'Give number of game board rows [3 or more]', 'Amount of rows must be at least 3"', '', '', '^[0-9]{1,}$')
+  rows = parseInt(ask('row', 'Give number of game board rows [3 or more]', 'Amount of rows must be at least 3"', '', '', '^[0-9]{1,}$'))
   console.log(rows)
-  cols = ask('col', 'Give number of game board columns  [3 or more]', 'Amount of columns must be at least 3"', '', '', '^[0-9]{1,}$')
+  cols = parseInt(ask('col', 'Give number of game board columns  [3 or more]', 'Amount of columns must be at least 3"', '', '', '^[0-9]{1,}$'))
   console.log(cols)
-  lineLength = ask(lineLength, 'Give number of items need to win the game [3 or more]', 'Winning line length must be at least 3"', 'Winning line length cannot be shorter than 3', 'Winning line cannot be longer than rows or columns.', '^[0-9]{1,}$')
-  console.log(lineLength)
+  lineLength = parseInt(ask('lineLength', 'Give number of items need to win the game [3 or more]', 'Winning line length must be at least 3"', 'Winning line length cannot be shorter than 3', 'Winning line cannot be longer than rows or columns.', '^[0-9]{1,}$'))
+  nbrPlayers = parseInt(ask('nbrPlayers', 'How many players [1 or 2]?', 'Select 1 or 2', '', '', '^[1-2]{1}$'))
+  player1 = ask('player1', 'Name of the Player 1', 'Name must contain alphabets only', '', '', '^[A-z]{2,}$')
+  if (nbrPlayers === 2) {
+    player2 = ask('player2', 'Name of the Player 2', 'Name must contain alphabets only', '', '', '^[A-z]{2,}$')
+  }
+
   setupBoard()
+  printBoard()
 }
 
 function ask (type, question, errormsg1, errormsg2, errormsg3, reg) {
@@ -29,11 +38,13 @@ function ask (type, question, errormsg1, errormsg2, errormsg3, reg) {
     if (!success) {
       console.log('incorrect input')
     } else {
+      // Checking that amount of rows and cols are correct
       if (type === 'row' || type === cols) {
         if (!success || value < 3) {
           console.log(errormsg1)
           success = !success
         }
+      // checking that winning line length is correct
       } else if (type === lineLength) {
         if (!success || (value < 3)) {
           console.log(errormsg2)
@@ -50,44 +61,46 @@ function ask (type, question, errormsg1, errormsg2, errormsg3, reg) {
 }
 
 function setupBoard () {
-/*  console.log('alusta taulukko')
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      gameBoard[i][j].push('_')
-    }
-  }
-*/
   for (let i = 0; i < rows; i++) {
     let data = []
     for (let j = 0; j < cols; j++) {
-      data.push('_')
+      data.push('   ')
     }
     gameBoard.push(data)
   }
+}
+
+function printRowLine () {
+  let line = '  '
+  for (let c = 1; c <= cols; c++) {
+    line += '--- '
+  }
+  console.log(line)
+}
+
+function printColHeads () {
+  let heads = '  '
+  for (let c = 1; c <= cols; c++) {
+    heads += ' ' + c + '  '
+  }
+  console.log(heads)
+}
+
+function printBoard () {
   console.log('tulosta taulukko: for-silmukka')
-  let output = ''
+  printColHeads()
+  printRowLine()
+  let output = '1 |'
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      output += gameBoard[i][j] + ' '
+      if (j === 0) {
+        output = (i + 1) + '|'
+      }
+      output += gameBoard[i][j] + '|'
     }
     console.log(output)
-    output = ''
+    printRowLine()
   }
-
-  /*  for (let i = 0; i < rows; i++) {
-//    let data = []
-    for (let j = 0; j < cols; j++) {
-//      data.push('_')
-
-    }
-    gameBoard.push(data)
-  }
-  for (let x = 0; x < gameBoard; x++) {
-    for (let item in gameBoard) {
-      console.log(gameBoard[item])
-    }
-  }
-*/
 }
 
 main()
