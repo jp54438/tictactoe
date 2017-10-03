1658702db94768da5539b0b401cd6f5607ea4c9b
jp54438 <juha.pekka.p@gmail.com>
2017-10-03 15:43:17 +0300

    basic game functionality without checking

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var rows = 3
 var cols = 3
 var lineLength = 3
 var gameBoard = []
-var player1 = ''
-var player2 = ''
+var players = [2]
 var nbrPlayers = 1
+var turn = 1
+var turnCount = 0
+var mark = ' X '
+var gameOver = false
+var nextPosition = []
 
 function main () {
   setupGame()
@@ -19,13 +23,12 @@ function setupGame () {
   console.log(cols)
   lineLength = parseInt(ask('lineLength', 'Give number of items need to win the game [3 or more]', 'Winning line length must be at least 3"', 'Winning line length cannot be shorter than 3', 'Winning line cannot be longer than rows or columns.', '^[0-9]{1,}$'))
   nbrPlayers = parseInt(ask('nbrPlayers', 'How many players [1 or 2]?', 'Select 1 or 2', '', '', '^[1-2]{1}$'))
-  player1 = ask('player1', 'Name of the Player 1', 'Name must contain alphabets only', '', '', '^[A-z]{2,}$')
+  players[0] = ask('player1', 'Name of the Player 1', 'Name must contain alphabets only', '', '', '^[A-z]{2,}$')
   if (nbrPlayers === 2) {
-    player2 = ask('player2', 'Name of the Player 2', 'Name must contain alphabets only', '', '', '^[A-z]{2,}$')
-  }
-
+    players[1] = ask('player2', 'Name of the Player 2', 'Name must contain alphabets only', '', '', '^[A-z]{2,}$')
+  } else players[1] = 'computer'
   setupBoard()
-  printBoard()
+  playGame()
 }
 
 function ask (type, question, errormsg1, errormsg2, errormsg3, reg) {
@@ -103,4 +106,47 @@ function printBoard () {
   }
 }
 
+function playGame () {
+  console.log('######### Game Begins #########')
+  // Arvotaan aloittaja
+  turn = Math.floor(Math.random() * 2)
+  console.log(turn)
+  if (turn === 0) {
+    console.log(players[0] + ' starts')
+  } else { // if (turn === 1)
+    console.log(players[1] + ' starts')
+  }
+  printBoard()
+
+  while (!gameOver) {
+    turnCount++
+    askPosition()
+    updateBoard(turn, nextPosition)
+    if (turn === 0) {
+      turn = 1
+    } else {
+      turn = 0
+    }
+    gameOver = checkWin()
+    printBoard()
+  }
+}
+
+function askPosition () {
+  nextPosition[0] = parseInt(ask('position', players[turn] + ': give column number [0 - ' + cols + ']', '', '', '', '^[0-9]{1}$'))
+  nextPosition[1] = parseInt(ask('position', players[turn] + ': give row number [0 - ' + rows + ']', '', '', '', '^[0-9]{1}$'))
+}
+function updateBoard () {
+  if (turn === 0) {
+    mark = ' X '
+  } else {
+    mark = ' 0 '
+  } 
+  let rowToUpdate = gameBoard[nextPosition[1] - 1]
+  rowToUpdate[nextPosition[0] - 1] = mark
+}
+
+function checkWin () {
+  return false
+}
 main()
